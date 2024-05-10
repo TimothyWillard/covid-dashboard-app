54992c1d8f4d029d222fc37fbcee7ff484217749
Timothy Willard <timothywillard22@gmail.com>
2024-05-10 13:59:19 -0400

    Convert Footer component to React 18 function interface

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'antd';
 import { ReactComponent as Logo } from '../assets/logo-john-hopkins.svg';
 import { styles } from '../utils/constants';
 
-class Footer extends Component {
-  
-    render() {
-        return (
-          <div id="footer" className="App-header">
-          <Row gutter={styles.gutter}>
-            <Col className="gutter-row" span={6}>
-              <Logo height="60"/>
-            </Col>
-            <Col className="gutter-row" offset={14} span={4}>
-              <div className="footer">© 2020 IDD Working Group</div>
-            </Col>
-          </Row>
-        </div>
-        )
-    }
+export default function Footer() {
+  return (
+    <div id="footer" className="App-header">
+      <Row gutter={styles.gutter}>
+        <Col className="gutter-row" span={6}>
+          <Logo height="60" />
+        </Col>
+        <Col className="gutter-row" offset={14} span={4}>
+          <div className="footer">© 2020 IDD Working Group</div>
+        </Col>
+      </Row>
+    </div>
+  )
 }
-
-export default Footer;
