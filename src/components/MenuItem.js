11507fd2fc4c9ab379bb65399a8241a8cefdbe0a
Timothy Willard <timothywillard22@gmail.com>
2024-05-10 15:43:51 -0400

    Convert MenuItem component to React 18 function interface

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ReactComponent as GraphLogo } from '../assets/graph.svg';
 import { ReactComponent as ChartLogo } from '../assets/chart.svg';
 import { ReactComponent as MapLogo } from '../assets/globe.svg';
@@ -6,62 +6,54 @@ import { ReactComponent as MethodsLogo } from '../assets/book.svg';
 import { ReactComponent as AboutLogo } from '../assets/info.svg';
 import { styles } from '../utils/constants';
 
-class MenuItem extends Component {
-
-  handleClickEvent = (menuItem) => {
-    this.props.handleMouseClick(menuItem)
-  }
-
-
-  render() {
-      const { menuItem, height, active, activeClass, hoverClass } = this.props;
-    
-      if (menuItem === 0) {
-        return( <GraphLogo 
+export default function MenuItem({menuItem, height, active, activeClass, hoverClass, handleMouseClick}) {
+  switch (menuItem) {
+    case 0:
+      return (
+        <GraphLogo 
           height={height}
           style={styles.Menu}
           className={active ? activeClass : hoverClass}
-          onClick={() => this.handleClickEvent(menuItem)}
+          onClick={() => handleMouseClick(menuItem)}
         />
-        )
-      }
-      if (menuItem === 1) {
-        return( <ChartLogo 
+      );
+    case 1:
+      return (
+        <ChartLogo 
           height={height}
           style={styles.Menu}
           className={active ? activeClass : hoverClass}
-          onClick={() => this.handleClickEvent(menuItem)}
+          onClick={() => handleMouseClick(menuItem)}
         />
-        )
-      }
-      if (menuItem === 2) {
-        return( <MapLogo 
-          height={height}
-          style={styles.MenuMap}
-          className={active ? activeClass : hoverClass}
-          onClick={() => this.handleClickEvent(menuItem)}
-        />
-        )
-      }
-      if (menuItem === 3) {
-        return( <MethodsLogo 
+      );
+    case 2:
+      return (
+        <MapLogo 
           height={height}
           style={styles.Menu}
           className={active ? activeClass : hoverClass}
-          onClick={() => this.handleClickEvent(menuItem)}
+          onClick={() => handleMouseClick(menuItem)}
         />
-        )
-      }
-      if (menuItem === 4) {
-        return( <AboutLogo 
+      );
+    case 3:
+      return (
+        <MethodsLogo 
           height={height}
           style={styles.Menu}
           className={active ? activeClass : hoverClass}
-          onClick={() => this.handleClickEvent(menuItem)}
+          onClick={() => handleMouseClick(menuItem)}
         />
-        )
-      }
+      );
+    case 4:
+      return (
+        <AboutLogo 
+          height={height}
+          style={styles.Menu}
+          className={active ? activeClass : hoverClass}
+          onClick={() => handleMouseClick(menuItem)}
+        />
+      );
+    default:
+      break;
   }
 }
-
-export default MenuItem;
