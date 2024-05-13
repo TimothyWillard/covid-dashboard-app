0b9a966bc0246a54c467cd36605532f3447f0026
Timothy Willard <timothywillard22@gmail.com>
2024-05-13 13:59:10 -0400

    Update the R0's Histogram component to the function interface instead of the class interface

--- src/components/Filters/Histogram.js
+++ src/components/Filters/Histogram.js
@@ -1,104 +1,81 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { bin, max, range } from 'd3-array';
-import { scaleLinear }  from 'd3-scale';
+import { scaleLinear } from 'd3-scale';
 import { colors } from '../../utils/colors.js';
 
-class Histogram extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: []
-    }
+export default function Histogram({ allSims, selectedSims, sortedSims, selected, r0min, r0max, height, step }) {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(document.querySelector('.r0-slider').clientWidth);
+    window.addEventListener('resize', (e) => {
+      const width = document.querySelector('.r0-slider').clientWidth;
+      setWidth(width);
+    });
+    return () => {
+      window.removeEventListener('resize', (e) => {
+        const width = document.querySelector('.r0-slider').clientWidth;
+        setWidth(width);
+      });
+    };
+  }, []);
+
+  const sorted_selected_sims = selectedSims.slice().sort((a, b) => a.r0 - b.r0);
+  const xScale = scaleLinear().domain([r0min, r0max]).range([0, width]).nice();
+  const yScale = scaleLinear().range([height, 1]);
+  const thresholds = range(r0min, r0max, step);
+
+  const binGenerator = bin()
+    .value(d => d.r0)
+    .domain(xScale.domain())
+    .thresholds(thresholds);
+
+  let bins = [];
+  let selectedBins = [];
+  if (sortedSims !== undefined) {
+    bins = binGenerator(sortedSims);
+    selectedBins = binGenerator(sorted_selected_sims);
+    yScale.domain([0, max(bins, d => d.length)]);
   }
 
-  componentDidMount() {
-    const width = document.querySelector('.r0-slider').clientWidth
-    this.setState({ width }, () => this.makeBins())
-    window.addEventListener('resize', this.updateHistogramWidth);
-  }
-
-  updateHistogramWidth = () => {
-    const width = document.querySelector('.r0-slider').clientWidth
-    this.setState({ width })
-  }
-
-  makeBins = () => {
-    const { sortedSims, selectedSims, r0min, r0max, step } = this.props;
-    const sorted_selected_sims = selectedSims.slice().sort((a,b) => a.r0 - b.r0)
-    const xScale = scaleLinear().domain([r0min, r0max]).range([0, this.state.width]).nice()
-    const yScale = scaleLinear().range([this.props.height, 1])
-    const thresholds = range(r0min, r0max, step)
-
-    const binGenerator = bin()
-        .value(d => d.r0)
-        .domain(xScale.domain())
-        .thresholds(thresholds)
-
-    const bins = binGenerator(sortedSims)
-    const selectedBins = binGenerator(sorted_selected_sims)
-
-    yScale.domain([0, max(bins, d => d.length)])
-    this.setState({ xScale, yScale, binGenerator, bins, selectedBins })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.width !== prevState.width) {
-      this.makeBins()
-    }
-    if (this.props.selectedSims !== prevProps.selectedSims || this.props.selected !== prevProps.selected) {
-      const sorted_selected_sims = this.props.selectedSims.slice().sort((a,b) => a.r0 - b.r0)
-      const selectedBins = this.state.binGenerator(sorted_selected_sims)
-      this.setState({ selectedBins })
-      this.makeBins()
-    }
-  }
-
-  render() {
-    const { xScale, yScale, bins, selectedBins } = this.state;
-      return (
-        <div>
-        {this.state.width && this.state.xScale &&
-          <svg width={this.state.width} height={this.props.height}>
-            <g>
-              {bins.map( (bin,i) => {
+  return (
+    <div>
+      {width && xScale &&
+        <svg width={width} height={height}>
+          <g>
+            {bins.map((b, i) => {
               return (
-                <rect 
+                <rect
                   key={`hist-${i}`}
-                  x={xScale(bin.x0)}
-                  y={yScale(bin.length)}
-                  width={xScale(bin.x1) - xScale(bin.x0)}
-                  height={this.props.height - yScale(bin.length)}
+                  x={xScale(b.x0)}
+                  y={yScale(b.length)}
+                  width={xScale(b.x1) - xScale(b.x0)}
+                  height={height - yScale(b.length)}
                   fill="#ffffff"
                   stroke={colors.chartBkgd}
                 >
                 </rect>
               )
             })}
-            </g>
-            <g>
-            {selectedBins.map( (bin,i) => {
+          </g>
+          <g>
+            {selectedBins.map((b, i) => {
               return (
-                <rect 
+                <rect
                   key={`hist-${i}`}
-                  x={xScale(bin.x0)}
-                  y={yScale(bin.length)}
-                  width={xScale(bin.x1) - xScale(bin.x0)}
-                  height={this.props.height - yScale(bin.length)}
+                  x={xScale(b.x0)}
+                  y={yScale(b.length)}
+                  width={xScale(b.x1) - xScale(b.x0)}
+                  height={height - yScale(b.length)}
                   fill={colors.sliderBlue}
                   stroke={colors.chartBkgd}
                 >
                 </rect>
               )
             })}
-            </g>
+          </g>
         </svg>
-        }
-        </div>
-        
-      )
-  }
-}
-
-export default Histogram
-
-
+      }
+    </div>
+  );
+};
