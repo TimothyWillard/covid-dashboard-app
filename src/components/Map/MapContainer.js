1d33eea6576a3f0f83b6ad1a1d2b776f0b0c65f1
Timothy Willard <timothywillard22@gmail.com>
2024-05-14 14:14:26 -0400

    Modernized the MapContainer component to use the function interface instead of the class interface, #167.

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,61 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Map from '../Map/Map';
 import { getDateIdx, getReadableDate, formatTitle } from '../../utils/utils';
 import { GEOIDS } from '../../utils/geoids.tsx';
 import { mapHighColorPalette, mapLowColorPalette } from '../../utils/colors';
 import { scalePow } from 'd3-scale';
 import { select } from 'd3-selection';
+import PropType from 'prop-types';
 
+const MapContainer = ({ geoid, indicators, width, height, scenario, firstDate, selectedDate, countyBoundaries, indicatorsForCounty, dateSliderActive }) => {
+    const [ strokeWidth, setStrokeWidth ] = useState(0.8);
+    const [ strokeHoverWidth, setStrokeHoverWidth ] = useState(1.8);
+    const [ children, setChildren ] = useState([]);
+    const mapContainerRef = useRef(null);
 
-class MapContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            children: [],
-            scaleColors: [],
-            strokeWidth: 0.8,
-            strokeHoverWidth: 1.8
-        }
-        this.mapRefContainer = React.createRef();
+    const strokeWidthScale = scalePow()
+        .exponent(0.25)
+        .range([0.1, 0.8])
+        .domain([9, 1]);
+    const strokeHoverWidthScale = scalePow()
+        .exponent(0.25)
+        .range([0.25, 1.8])
+        .domain([9, 1]);
 
-        this.strokeWidthScale = scalePow().exponent(0.25)
-            .range([0.1, 0.8]).domain([9, 1])
-        this.strokeHoverWidthScale = scalePow().exponent(0.25)
-            .range([0.25, 1.8]).domain([9, 1])
-    }
-
-    componentDidMount() {
-        const { geoid, scenario, firstDate, selectedDate, width, height } = this.props;
-        this.initializeMaps(geoid, scenario, firstDate, selectedDate, width, height);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-        if (this.props.geoid !== prevProps.geoid
-            || this.props.selectedDate !== prevProps.selectedDate
-            || this.props.countyBoundaries !== prevProps.countyBoundaries
-            || this.props.scenario !== prevProps.scenario
-            || this.props.width !== prevProps.width
-            || this.props.height !== prevProps.height
-            || this.state.strokeWidth !== prevState.strokeWidth) {
-
-            const { geoid, scenario, firstDate, selectedDate, width, height } = this.props;
-            this.initializeMaps(geoid, scenario, firstDate, selectedDate, width, height);
+    function handleZoom(event) {
+        if (mapContainerRef.current) {
+            const mapNode = select(mapContainerRef.current);
+            mapNode.selectAll('path')
+                .attr('transform', event.transform);
+            const strokeWidth = strokeWidthScale(event.transform.k);
+            const strokeHoverWidth = strokeHoverWidthScale(event.transform.k);
+            setStrokeWidth(strokeWidth);
+            setStrokeHoverWidth(strokeHoverWidth);
         }
     }
 
-    initializeMaps = (geoid, scenario, firstDate, selectedDate, width, height) => {
+    function initializeMaps(geoid, scenario, firstDate, selectedDate, width, height) {
         const children = [];
-        const { indicators } = this.props;
-        const { strokeWidth, strokeHoverWidth } = this.state;
         const dateIdx = getDateIdx(firstDate, selectedDate);
-        let divider;
+        let divider = 3;
         if (width < 350) {
             divider = 1
         } else if (width >= 350 && width < 700) {
             divider = 2
-        } else {
-            divider = 3
         }
 
         for (let indicator of indicators) {
@@ -70,60 +56,67 @@ class MapContainer extends Component {
                     geoid={geoid}
                     scenario={scenario}
                     dateIdx={dateIdx}
-                    countyBoundaries={this.props.countyBoundaries}
-                    indicatorsForCounty={this.props.indicatorsForCounty}
+                    countyBoundaries={countyBoundaries}
+                    indicatorsForCounty={indicatorsForCounty}
                     width={width / divider}
                     height={height}
                     lowColor={mapLowColorPalette[indicator.id]}
                     highColor={mapHighColorPalette[indicator.id]}
                     strokeWidth={strokeWidth}
                     strokeHoverWidth={strokeHoverWidth}
-                    handleZoom={this.handleZoom}
+                    handleZoom={handleZoom}
                 />
             )
             children.push(child);
         }
-        this.setState({ children });
+
+        setChildren(children);
     }
 
-    handleZoom = (event) => {
-        if (this.mapRefContainer.current) {
-            const mapNode = select(this.mapRefContainer.current)
-            mapNode.selectAll('path')
-                .attr('transform', event.transform)
-            const strokeWidth = this.strokeWidthScale(event.transform.k)
-            const strokeHoverWidth = this.strokeHoverWidthScale(event.transform.k)
-            this.setState({ strokeWidth, strokeHoverWidth })
-        }
-    }
+    /* eslint-disable react-hooks/exhaustive-deps */
+    useEffect(() => {
+        initializeMaps(geoid, scenario, firstDate, selectedDate, width, height);
+    }, [ geoid, scenario, firstDate, selectedDate, width, height ]);
+    /* eslint-enable react-hooks/exhaustive-deps */
 
-    render() {
-        const { geoid, scenario } = this.props;
-        const scenarioTitle = formatTitle(scenario);
-        const geoidName = `${GEOIDS[geoid]}`;
-        return (
-            <div>
-                <div className="scenario-title titleNarrow">{geoidName}</div>
-                <div className="scenario-title">{scenarioTitle}</div>
-                <div className="filter-label threshold-label callout callout-row">
-                    {`Snapshot on `}
-                    {/*TS migration: Use getClassForActiveState(this.props.dateSliderActive)*/}
-                    <span className={this.props.dateSliderActive ? 'underline-active' : 'bold underline'}>
-                        {getReadableDate(this.props.selectedDate)}
-                    </span>
-                </div>
-                <div className="map-wrapper" ref={this.mapRefContainer}>
-                    {this.state.children.map(child => {
-                        return (
-                            <div className="map" key={child.key}>
-                                {child.map}
-                            </div>
-                        )
-                    })}
-                </div>
+    const scenarioTitle = formatTitle(scenario);
+    const geoidName = `${GEOIDS[geoid]}`;
+
+    return (
+        <div>
+            <div className="scenario-title titleNarrow">{geoidName}</div>
+            <div className="scenario-title">{scenarioTitle}</div>
+            <div className="filter-label threshold-label callout callout-row">
+                {`Snapshot on `}
+                {/*TS migration: Use getClassForActiveState(this.props.dateSliderActive)*/}
+                <span className={dateSliderActive ? 'underline-active' : 'bold underline'}>
+                    {getReadableDate(selectedDate)}
+                </span>
             </div>
-        )
-    }
+            <div className="map-wrapper" ref={mapContainerRef}>
+                {children.map(child => {
+                    return (
+                        <div className="map" key={child.key}>
+                            {child.map}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+MapContainer.propTypes = {
+    geoid: PropType.string.isRequired,
+    indicators: PropType.array.isRequired,
+    width: PropType.number.isRequired,
+    height: PropType.number.isRequired,
+    scenario: PropType.string.isRequired,
+    firstDate: PropType.string.isRequired,
+    selectedDate: PropType.string.isRequired,
+    countyBoundaries: PropType.object.isRequired,
+    indicatorsForCounty: PropType.object.isRequired,
+    dateSliderActive: PropType.bool.isRequired
 }
 
-export default MapContainer
+export default MapContainer;
