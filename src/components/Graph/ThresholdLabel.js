9b09ddf720ab6371528fc161a1b117285115872f
Timothy Willard <timothywillard22@gmail.com>
2024-05-16 10:07:06 -0400

    Updated the ThresholdLabel to use React's function interface, #167

--- src/components/Graph/ThresholdLabel.js
+++ src/components/Graph/ThresholdLabel.js
@@ -1,78 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { InfoCircleTwoTone } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+
 import TooltipHandler from '../Filters/TooltipHandler';
 import { addCommas, getReadableDate, getStepValue } from '../../utils/utils';
-import { getClassForActiveState, LabelClassNameEnum } from "../../utils/typeUtils";
+import { getClassForActiveState } from "../../utils/typeUtils";
 
-class ThresholdLabel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTooltip: false,
-            chance: Math.round(100 * props.percExceedence),
-            val: addCommas(Math.ceil(props.indicatorThreshold / getStepValue(props.seriesMax)) * getStepValue(props.seriesMax)),
-            date: getReadableDate(props.dateThreshold),
-            activeClass: LabelClassNameEnum.boldUnderline,
-            statClass: LabelClassNameEnum.boldUnderline,
-            dateClass: LabelClassNameEnum.boldUnderline
-        }
+const ThresholdLabel = ({ classProps, indicatorThreshold, seriesMax, dateThreshold, percExceedence, label, statSliderActive, dateSliderActive }) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
+    
+    const handleTooltipClick = () => {
+        setShowTooltip(!showTooltip);
     }
 
-    componentDidUpdate(prevProps) {
-        const { percExceedence, indicatorThreshold, dateThreshold, statSliderActive, dateSliderActive, seriesMax } = this.props;
+    const activeClass = getClassForActiveState(statSliderActive || dateSliderActive);
+    const dateClass = getClassForActiveState(dateSliderActive);
+    const statClass = getClassForActiveState(statSliderActive);
+    const chance = Math.round(100 * percExceedence);
 
-        if (percExceedence !== prevProps.percExceedence) {
-            this.setState({ chance: Math.round(100 * percExceedence) });
-        }
-        if (indicatorThreshold !== prevProps.indicatorThreshold) {
-            const stepVal = getStepValue(seriesMax);
-            const roundedStat = Math.ceil(indicatorThreshold / stepVal) * stepVal;
-            this.setState({ val: addCommas(roundedStat) });
-        }
-        if (dateThreshold !== prevProps.dateThreshold) {
-            this.setState({ date: getReadableDate(dateThreshold) });
-        }
-        if (statSliderActive !== prevProps.statSliderActive || dateSliderActive !== prevProps.dateSliderActive) {
-            this.setState({
-                activeClass: getClassForActiveState(statSliderActive || dateSliderActive),
-                statClass: getClassForActiveState(statSliderActive),
-                dateClass: getClassForActiveState(dateSliderActive),
-            });
-        }
-    }
+    const stepVal = getStepValue(seriesMax);
+    const roundedStat = Math.ceil(indicatorThreshold / stepVal) * stepVal;
+    const val = addCommas(roundedStat);
+    const date = getReadableDate(dateThreshold);
 
-    handleTooltipClick = () => {
-        this.setState({ showTooltip: !this.state.showTooltip });
-    }
+    return (
+        <div className={`${classProps} desktop-only`}>
+            <span className={activeClass}>{chance}%</span>
+            &nbsp;{`of simulations shown predict daily ${label} to exceed`}&nbsp;
+            <span className={statClass}>{val}</span>
+            &nbsp;by&nbsp;
+            <span className={dateClass}>{date}</span>
+            <TooltipHandler
+                showTooltip={showTooltip}
+                onClick={handleTooltipClick}
+                >
+                <div className="tooltip">
+                    &nbsp;<InfoCircleTwoTone />
+                    {showTooltip &&
+                    <span className="tooltip-text">
+                        This percentage is calculated only for model simulations that 
+                        meet the criteria specified in the control panel. 
+                        Modify the Threshold and Date Threshold sliders in 
+                        “Threshold Exceedance” mode to see how this percentage changes.
+                    </span> }
+                </div>
+            </TooltipHandler>
+        </div>
+    );
+};
 
-    render() {
-        const { chance, val, date, activeClass, statClass, dateClass } = this.state;
-
-        return (
-            <div className={`${this.props.classProps} desktop-only`}>
-                <span className={activeClass}>{chance}%</span>
-                &nbsp;{`of simulations shown predict daily ${this.props.label} to exceed`}&nbsp;
-                <span className={statClass}>{val}</span>
-                &nbsp;by&nbsp;
-                <span className={dateClass}>{date}</span>
-                <TooltipHandler
-                    showTooltip={this.state.showTooltip}
-                    onClick={this.handleTooltipClick}
-                    >
-                    <div className="tooltip">
-                        &nbsp;<InfoCircleTwoTone />
-                        {this.state.showTooltip &&
-                        <span className="tooltip-text">
-                            This percentage is calculated only for model simulations that 
-                            meet the criteria specified in the control panel. 
-                            Modify the Threshold and Date Threshold sliders in 
-                            “Threshold Exceedance” mode to see how this percentage changes.
-                        </span> }
-                    </div>
-                </TooltipHandler>
-            </div>
-        )
-    }
-}
+ThresholdLabel.propTypes = {
+    classProps: PropTypes.string.isRequired,
+    indicatorThreshold: PropTypes.number.isRequired,
+    seriesMax: PropTypes.number.isRequired,
+    dateThreshold: PropTypes.instanceOf(Date).isRequired,
+    percExceedence: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+    statSliderActive: PropTypes.bool.isRequired,
+    dateSliderActive: PropTypes.bool.isRequired,
+};
 
 export default ThresholdLabel;
