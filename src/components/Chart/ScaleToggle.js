e20ced410a77781fcd0d287ac9cfdaa9a5ae5344
Timothy Willard <timothywillard22@gmail.com>
2024-05-21 12:40:57 -0400

    Updated the ScaleToggle to use React's function interface instead of the class interface.

--- src/components/Chart/ScaleToggle.js
+++ src/components/Chart/ScaleToggle.js
@@ -1,58 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Radio } from 'antd';
 import { InfoCircleTwoTone } from '@ant-design/icons';
-import { styles } from '../../utils/constants';
+import PropTypes from 'prop-types';
+
 import TooltipHandler from '../Filters/TooltipHandler';
+
+import { styles } from '../../utils/constants';
 
 const ScaleTypeEnum = {
     linear: 'linear',
     power: 'power'
 };
 
-class ScaleToggle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTooltip: false
-        };
-    }
+const ScaleToggle = ({ scale, onScaleToggle }) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
 
-    handleTooltipClick = () => {
-        this.setState({ showTooltip: !this.state.showTooltip });
-    }
+    const handleTooltipClick = useCallback(() => {
+        setShowTooltip(!showTooltip);
+    }, [ showTooltip ]);
 
-    handleChange = (e) => {
-        this.props.onScaleToggle(e.target.value);
-    };
+    const handleScaleChange = useCallback((event) => {
+        onScaleToggle(event.target.value);
+    }, [ onScaleToggle ]);
 
-    render() {
-        return (
-            <div>
-                <div className="param-header">Y-AXIS SCALE
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                    >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                                <span className="tooltip-text">
-                                    Toggle between a linear scale or a power scale, 
-                                    which reveals more granularity at lower levels.
-                                </span>}
-                        </div>
-                    </TooltipHandler>
-                </div>
-                <Radio.Group
-                    value={this.props.scale}
-                    style={styles.Selector}
-                    onChange={this.handleChange}>
-                    <Radio.Button value={ScaleTypeEnum.linear}>Linear</Radio.Button>
-                    <Radio.Button value={ScaleTypeEnum.power}>Power</Radio.Button>
-                </Radio.Group>
+    return (
+        <div>
+            <div className="param-header">
+                Y-AXIS SCALE
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}>
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                        <span className="tooltip-text">
+                            Toggle between a linear scale or a power scale, 
+                            which reveals more granularity at lower levels.
+                        </span>}
+                    </div>
+                </TooltipHandler>
             </div>
-        );
-    }
-}
+            <Radio.Group
+                value={scale}
+                style={styles.Selector}
+                onChange={handleScaleChange}>
+                <Radio.Button value={ScaleTypeEnum.linear}>Linear</Radio.Button>
+                <Radio.Button value={ScaleTypeEnum.power}>Power</Radio.Button>
+            </Radio.Group>
+        </div>
+    );
+};
+
+ScaleToggle.propTypes = {
+    scale: PropTypes.string.isRequired,
+    onScaleToggle: PropTypes.func.isRequired,
+};
 
 export default ScaleToggle;
