8ad3fd9b0928a2506f609ab6e862c814670a53b7
Timothy Willard <timothywillard22@gmail.com>
2024-05-21 15:06:56 -0400

    Modernized the ChartContainer component to use React's function interface instead of the class interface

--- src/components/Chart/ChartContainer.js
+++ src/components/Chart/ChartContainer.js
@@ -1,44 +1,57 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useCallback } from 'react';
 import { Row, Col } from 'antd';
-import Chart from '../Chart/Chart';
-import CalloutLabel from '../Chart/CalloutLabel';
-import ChartLegend from '../Chart/ChartLegend';
+import PropTypes from 'prop-types';
+
+import Chart from './Chart';
+import CalloutLabel from './CalloutLabel';
+import ChartLegend from './ChartLegend';
+
 import { GEOIDS } from '../../utils/geoids.tsx';
 import { getReadableDate, formatTitle } from '../../utils/utils';
 
-class ChartContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataLoaded: false,
-            children: {},
-            selectedIndicators: {},
-            hoveredScenarioIdx: null
+const ChartContainer = ({
+    geoid,
+    width,
+    height,
+    dataset,
+    scenarios,
+    scenarioMap,
+    indicators,
+    firstDate,
+    start,
+    end,
+    scale,
+    datePickerActive
+}) => {
+    const [ hoveredScenarioIdx, setHoveredScenarioIdx ] = useState(null);
+    const [ calloutStats, setCalloutStats ] = useState({
+        statLabel: '',
+        median: 0,
+        tenth: 0,
+        ninetyith: 0,
+    });
+
+    const handleCalloutInfo = useCallback((statLabel, median, tenth, ninetyith) => {
+        setCalloutStats({
+            statLabel: statLabel.toLowerCase(),
+            median: median,
+            tenth: tenth,
+            ninetyith: ninetyith,
+        });
+    }, []);
+
+    const handleCalloutLeave = () => {};
+
+    const handleScenarioHighlight = useCallback((scenarioIdx) => {
+        if (scenarioIdx !== null) {
+            setHoveredScenarioIdx(scenarioIdx);
+        } else {
+            setHoveredScenarioIdx(null);
         }
-    }
+    }, []);
 
-    componentDidMount() {
-        this.drawCharts();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.start !== this.props.start
-            || prevProps.end !== this.props.end
-            || prevProps.dataset !== this.props.dataset
-            || prevProps.scenarios !== this.props.scenarios
-            || prevProps.indicators !== this.props.indicators
-            || prevProps.scale !== this.props.scale
-            || prevProps.width !== this.props.width
-            || prevProps.height !== this.props.height) {
-
-            this.drawCharts();
-        }
-    }
-
-    drawCharts = () => {
-        const { children } = this.state;
-        const { indicators, dataset, scenarios, width, height, scale } = this.props;
-
+    let children = [];
+    if (indicators && indicators.length > 0) {
         for (let indicator of indicators) {
             const child = {
                 key: `${indicator.key}-chart`,
@@ -49,102 +62,92 @@ class ChartContainer extends Component {
                     key={`${indicator.key}-chart`}
                     dataset={dataset}
                     scenarios={scenarios}
-                    scenarioMap={this.props.scenarioMap}
-                    firstDate={this.props.firstDate}
-                    start={this.props.start}
-                    end={this.props.end}
+                    scenarioMap={scenarioMap}
+                    firstDate={firstDate}
+                    start={start}
+                    end={end}
                     indicator={indicator.key}
                     statLabel={indicator.name}
                     indicators={indicators}
                     width={width}
-                    height={height / Object.keys(indicators).length}
-                    handleCalloutInfo={this.handleCalloutInfo}
-                    handleCalloutLeave={this.handleCalloutLeave}
-                    handleScenarioHover={this.handleScenarioHighlight}
-                    scale={scale}
-                />
+                    height={height/Object.keys(indicators).length}
+                    handleCalloutInfo={handleCalloutInfo}
+                    handleCalloutLeave={handleCalloutLeave}
+                    handleScenarioHover={handleScenarioHighlight}
+                    scale={scale}/>
             children[indicator.key] = child;
         }
-        this.setState({children, selectedIndicators: indicators}, () => {
-            this.setState({
-                dataLoaded: true
-            });
-        })
     }
 
-    handleCalloutInfo = (statLabel, median, tenth, ninetyith) => {
-        this.setState({ statLabel, median, tenth, ninetyith, rectIsHovered: true });
-    }
+    const geoidName = `${GEOIDS[geoid]}`;
 
-    handleCalloutLeave = () => {
-        this.setState({ rectIsHovered: false })
-    }
-
-    handleScenarioHighlight = (scenarioIdx) => {
-        if (scenarioIdx !== null) {
-            this.setState({ hoveredScenarioIdx: scenarioIdx })
-        } else {
-            this.setState({ hoveredScenarioIdx: null })
-        }
-    }
-
-    render() {
-        const { hoveredScenarioIdx } = this.state;
-        const { geoid, scenarios, datePickerActive } = this.props;
-        const geoidName = `${GEOIDS[geoid]}`;
-        return (
-            <Fragment>
-                <Row>
-                    <Col span={24}>
-                        <div className="scenario-title titleNarrow">{geoidName}</div>
-                        <div className="filter-label threshold-label callout callout-row">
-                            {`Snapshot from `}
-                            {/*TS migration: Use getClassForActiveState(datePickerActive)*/}
-                            <span className={datePickerActive ? 'underline-active' : 'bold underline'}>
-                                {getReadableDate(this.props.start)}</span>&nbsp;to&nbsp;
-                            {/*TS migration: Use getClassForActiveState(datePickerActive)*/}
-                            <span className={datePickerActive ? 'underline-active' : 'bold underline'}>
-                                {getReadableDate(this.props.end)}</span>
-                        </div>
-                    </Col>
-                </Row>
-
-                <Row justify="end">
-                    <div className="widescreen-only">
-                        <div className="chart-callout" style={{ display: 'block !important'}}>
-                            {hoveredScenarioIdx !== null &&
-                                <CalloutLabel
-                                    classProps={'filter-label callout'}
-                                    start={this.props.start}
-                                    end={this.props.end}
-                                    scenario={formatTitle(scenarios[hoveredScenarioIdx])}
-                                    label={this.state.statLabel.toLowerCase()}
-                                    median={this.state.median}
-                                    tenth={this.state.tenth}
-                                    ninetyith={this.state.ninetyith}
-                                />
-                            }
-                        </div>
+    return (
+        <Fragment>
+            <Row>
+                <Col span={24}>
+                    <div className="scenario-title titleNarrow">{geoidName}</div>
+                    <div className="filter-label threshold-label callout callout-row">
+                        {`Snapshot from `}
+                        {/*TS migration: Use getClassForActiveState(datePickerActive)*/}
+                        <span className={datePickerActive ? 'underline-active' : 'bold underline'}>
+                            {getReadableDate(start)}
+                        </span>
+                        &nbsp;to&nbsp;
+                        {/*TS migration: Use getClassForActiveState(datePickerActive)*/}
+                        <span className={datePickerActive ? 'underline-active' : 'bold underline'}>
+                            {getReadableDate(end)}
+                        </span>
                     </div>
-
-                    <div className="chart-legend-container">
-                        <ChartLegend />
+                </Col>
+            </Row>
+            <Row justify="end">
+                <div className="widescreen-only">
+                    <div className="chart-callout" style={{ display: 'block !important'}}>
+                        {hoveredScenarioIdx !== null &&
+                            <CalloutLabel
+                                classProps={'filter-label callout'}
+                                start={start}
+                                end={end}
+                                scenario={formatTitle(scenarios[hoveredScenarioIdx])}
+                                label={calloutStats.statLabel}
+                                median={calloutStats.median}
+                                tenth={calloutStats.tenth}
+                                ninetyith={calloutStats.ninetyith}/>
+                        }
                     </div>
-                </Row>
-                <Row>
-                {this.state.dataLoaded && this.state.selectedIndicators.map(indicator => {
+                </div>
+                <div className="chart-legend-container">
+                    <ChartLegend />
+                </div>
+            </Row>
+            <Row>
+                {indicators && indicators.length > 0 && indicators.map((indicator) => {
                     return (
                         <div className="row" key={`chart-row-${indicator.key}`}>
                             <div className="chart" key={`chart-${indicator.key}`}>
-                                {this.state.children[indicator.key].chart}
+                                {children[indicator.key].chart}
                             </div>
                         </div>
-                    )
+                    );
                 })}
-                </Row>
-            </Fragment>
-        )
-    }
-}
+            </Row>
+        </Fragment>
+    );
+};
 
-export default ChartContainer
+ChartContainer.propTypes = {
+    geoid: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    dataset: PropTypes.object.isRequired,
+    scenarios: PropTypes.array.isRequired,
+    scenarioMap: PropTypes.object.isRequired,
+    indicators: PropTypes.array.isRequired,
+    firstDate: PropTypes.instanceOf(Date).isRequired,
+    start: PropTypes.instanceOf(Date).isRequired,
+    end: PropTypes.instanceOf(Date).isRequired,
+    scale: PropTypes.string.isRequired,
+    datePickerActive: PropTypes.bool.isRequired,
+};
+
+export default ChartContainer;
