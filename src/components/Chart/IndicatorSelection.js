9bc2d389baa3c320063d3c1244880e6bd692a1ed
Timothy Willard <timothywillard22@gmail.com>
2024-05-21 13:05:37 -0400

    Update the IndicatorSelection component to use the React function interface instead of the class interface

--- src/components/Chart/IndicatorSelection.js
+++ src/components/Chart/IndicatorSelection.js
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Select } from 'antd';
 import { InfoCircleTwoTone } from '@ant-design/icons';
-import { styles } from '../../utils/constants';
+import PropTypes from 'prop-types';
+
 import TooltipHandler from '../Filters/TooltipHandler';
 
-class IndicatorSelection extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            indicatorsForChart: [],
-            showTooltip: false,
-            children: []
-        }
-    }
+import { styles } from '../../utils/constants';
 
-    componentDidMount() {
-        const children = [];
-        const { Option } = Select;
-        const { statList, indicators } = this.props;
+const { Option } = Select;
+
+const IndicatorSelection = ({ indicators, statList, onStatClickChart }) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
+
+    const handleTooltipClick = useCallback(() => {
+        setShowTooltip(!showTooltip);
+    }, [ showTooltip ]);
+
+    const handleChange = useCallback((event) => {
+        if (event.length === 1)
+            return;
+        onStatClickChart(event);
+    }, [ onStatClickChart ]);
+
+    let indicatorsForChart = [];
+    let children = []
+    if (statList && statList.length > 0) {
+        const keys = Object
+            .values(statList)
+            .map(indicator => indicator.key);
+        indicatorsForChart = Array.from(indicators);
         
-        const indicatorsForChart = Array.from(indicators);
-        const keys = Object.values(statList).map(indicator => indicator.key);
-        
-        if (statList.length >= 2) {
-          indicatorsForChart.map(indicator => {
-            if (keys.includes(indicator.key)) {
-                return indicator.disabled = false;
-            } else {
-              return indicator.disabled = true;
-              }
-          })
+        if (statList.length >= 3) {
+            indicatorsForChart.map(indicator => {
+                if (keys.includes(indicator.key)) {
+                    return indicator.disabled = false;
+                }
+                return indicator.disabled = true;
+            })
         } else {
-          indicatorsForChart.map(indicator => {return indicator.disabled = false})
+            indicatorsForChart.map(indicator => {
+                return indicator.disabled = false
+            })
         }
 
         for (let indicator of indicatorsForChart) {
@@ -41,105 +50,50 @@ class IndicatorSelection extends Component {
             } 
             child.checkbox.push(
                 <Option
-                    key={indicator.key}>
+                    key={indicator.key}
+                    disabled={indicator.disabled}>
                     {indicator.name}
                 </Option>
             )
             children.push(child);
         }
-
-        this.setState({
-            indicatorsForChart,
-            children,
-        })
     }
 
-    componentDidUpdate(prevProp) {
-      if (prevProp.statList !== this.props.statList) {
-          const { statList, indicators } = this.props;
-
-          const keys = Object.values(statList).map(indicator => indicator.key);
-          const indicatorsForChart = Array.from(indicators);
-          
-          if (statList.length >= 3) {
-            indicatorsForChart.map(indicator => {
-              if (keys.includes(indicator.key)) {
-                  return indicator.disabled = false;
-              } else {
-                return indicator.disabled = true;
-              }
-            })
-          } else {
-            indicatorsForChart.map(indicator => {return indicator.disabled = false})
-          }
-
-
-          const children = [];
-          const { Option } = Select;
-  
-          for (let indicator of indicatorsForChart) {
-              const child = {
-                  key: indicator.key,
-                  checkbox: []
-              } 
-              child.checkbox.push(
-                  <Option
-                      key={indicator.key}
-                      disabled={indicator.disabled}>
-                      {indicator.name}
-                  </Option>
-              )
-              children.push(child);
-          }
-
-          this.setState({
-              indicatorsForChart,
-              children
-          })
-      }
-    }
-
-    handleTooltipClick = () => {
-        this.setState({showTooltip: !this.state.showTooltip})
-    }
-
-    handleChange = (event) => {
-        // prevent user from deselecting all scenarios
-        if (event.length === 1) { return };
-        this.props.onStatClickChart(event);
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="param-header">INDICATORS
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                        >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                            <span className="tooltip-text">
-                            Indicators are calculated after accounting for the 
-                            appropriate time delays and probabilities of transitioning 
-                            into a given state (e.g., initial infection to hospitalization). 
-                            </span> }
-                        </div>
-                    </TooltipHandler>
-                </div>
-                <Select
-                    mode="multiple"
-                    style={styles.Selector}
-                    defaultValue={this.props.statList.map(s => s.key)}
-                    value={this.props.statList.map(s => s.key)}
-                    maxTagTextLength={12}
-                    onChange={this.handleChange}>
-                    {this.state.children.map(child => child.checkbox)}
-                </Select>
+    return (
+        <div>
+            <div className="param-header">
+                INDICATORS
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}>
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                        <span className="tooltip-text">
+                        Indicators are calculated after accounting for the 
+                        appropriate time delays and probabilities of transitioning 
+                        into a given state (e.g., initial infection to hospitalization). 
+                        </span>}
+                    </div>
+                </TooltipHandler>
             </div>
-        )
-    }
-}
+            <Select
+                mode="multiple"
+                style={styles.Selector}
+                defaultValue={statList.map(s => s.key)}
+                value={statList.map(s => s.key)}
+                maxTagTextLength={12}
+                onChange={handleChange}>
+                {children.map(child => child.checkbox)}
+            </Select>
+        </div>
+    );
+};
 
-export default IndicatorSelection
+IndicatorSelection.propTypes = {
+    indicators: PropTypes.array.isRequired,
+    statList: PropTypes.array.isRequired,
+    onStatClickChart: PropTypes.func.isRequired,
+};
+
+export default IndicatorSelection;
