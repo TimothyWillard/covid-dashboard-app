366158672cff3a9bc2e6ad593b849c722bdf77ed
Timothy Willard <timothywillard22@gmail.com>
2024-05-28 12:35:17 -0400

    Updated the ActualSwitch component to use React's function interface instead of the class interface

--- src/components/Filters/ActualSwitch.js
+++ src/components/Filters/ActualSwitch.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Row, Switch } from 'antd';
 import { InfoCircleTwoTone } from '@ant-design/icons';
 import TooltipHandler from './TooltipHandler';
 import { styles } from '../../utils/constants';
 
-class ActualSwitch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTooltip: false
-        };
-    }
+const ActualSwitch = ({ showActual, actualList, onChange }) => {
+    const [showTooltip, setShowTooltip] = useState(false);
 
-    handleTooltipClick = () => {
-        this.setState({ showTooltip: !this.state.showTooltip });
+    const handleTooltipClick = () => {
+        setShowTooltip(!showTooltip);
     };
 
-    render() {
-        const { showActual, actualList } = this.props;
-        // assumes ground truth data exists for all scenarios if it exists for one
-        const isDisabled = actualList[0].length === 0;
-        return (
-            <Row gutter={styles.gutter} style={styles.Switch}>
-                <Switch
-                    style={{ marginTop: '0.1rem' }}
-                    checked={showActual}
-                    onChange={this.props.onChange}
-                    disabled={isDisabled}
-                    size="small"
-                />
-                <div className="upload-toggle">REPORTED DATA
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                    >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                                <span className="tooltip-text">
-                                    Daily reported confirmed cases and deaths are from USA Facts.
-                                    If the toggle is disabled, then data is unavailable for the selected indicator.
-                                </span>}
-                        </div>
-                    </TooltipHandler>
-                </div>
-            </Row>
-        );
-    }
-}
+    // assumes ground truth data exists for all scenarios if it exists for one
+    const isDisabled = actualList[0].length === 0;
+
+    return (
+        <Row gutter={styles.gutter} style={styles.Switch}>
+            <Switch
+                style={{ marginTop: '0.1rem' }}
+                checked={showActual}
+                onChange={onChange}
+                disabled={isDisabled}
+                size="small"
+            />
+            <div className="upload-toggle">REPORTED DATA
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}
+                >
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                            <span className="tooltip-text">
+                                Daily reported confirmed cases and deaths are from USA Facts.
+                                If the toggle is disabled, then data is unavailable for the selected indicator.
+                            </span>}
+                    </div>
+                </TooltipHandler>
+            </div>
+        </Row>
+    );
+};
 
 export default ActualSwitch;
