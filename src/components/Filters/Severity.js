c6af5455e8d30280ad543e41495554eafbb7a0e8
Timothy Willard <timothywillard22@gmail.com>
2024-05-28 15:39:38 -0400

    Modernize the Severity component to use React's function interface instead of its class interface

--- src/components/Filters/Severity.js
+++ src/components/Filters/Severity.js
@@ -1,106 +1,106 @@
-import React, { Component } from 'react';
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Radio } from 'antd';
 import { InfoCircleTwoTone } from '@ant-design/icons';
-import TooltipHandler from '../Filters/TooltipHandler';
 import _ from 'lodash';
-import { LEVELS, styles } from '../../utils/constants.js';
-import { capitalize, formatTitle } from '../../utils/utils.js';
 
-class Severity extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            children: [],
-            showTooltip: false
-        }
-    }
+import TooltipHandler from './TooltipHandler';
+import { LEVELS, styles } from '../../utils/constants';
+import { capitalize, formatTitle } from '../../utils/utils';
 
-    componentDidMount() {
-        const { existingSevs } = this.props;
-        this.initialize(existingSevs);
-    }
+const Severity = ({ 
+    existingSevs, 
+    scenario, 
+    onSeverityClick, 
+    onSeverityHover, 
+    onSeverityHoverLeave, 
+    isDisabled, 
+    sevCount, 
+    severity 
+}) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
 
-    componentDidUpdate(prevProp) {
-        const { existingSevs } = this.props;
-        if (prevProp.existingSevs !== existingSevs ) {
-            this.initialize(existingSevs);
-        }
-    }
-
-    initialize = (existingSevs) => {
-        const children = [];
-    
-        for (let level of LEVELS) {
-            const child = {
-                key: `${level.key}-severity`,
-                button: []
-            } 
-            child.button.push(
-                <Radio.Button
-                    key={`${level.key}-severity`}
-                    // disable radio button if severity level does not exist
-                    disabled={!existingSevs.includes(level.key)}
-                    value={level.key}>{capitalize(level.key)}
-                </Radio.Button>
-            )
-            children.push(child);
-        }
-        this.setState({children})
-    }
-
-    handleChange = (e) => {
-        // add scenario to obj so MainContainer knows which scenario is active
+    const handleChange = useCallback((e) => {
         const item = LEVELS.filter(level => level.key === e.target.value)[0];
         const itemClone = _.assign({}, item, {
-            scenario: this.props.scenario.key
+            scenario: scenario.key
         });
-        this.props.onSeverityClick(itemClone);
+        onSeverityClick(itemClone);
+    }, [ scenario, onSeverityClick ]);
+
+    const handleTooltipClick = useCallback(() => {
+        setShowTooltip(!showTooltip);
+    }, [ showTooltip ]);
+
+    const handleMouseEnter = useCallback((e) => {
+        onSeverityHover(e)
+    }, [ onSeverityHover ]);
+
+    const handleMouseLeave = useCallback((e) => {
+        onSeverityHoverLeave(e)
+    }, [ onSeverityHoverLeave ]);
+
+    let children = [];
+    for (let level of LEVELS) {
+        const child = {
+            key: `${level.key}-severity`,
+            button: []
+        } 
+        child.button.push(
+            <Radio.Button
+                key={`${level.key}-severity`}
+                // disable radio button if severity level does not exist
+                disabled={!existingSevs.includes(level.key)}
+                value={level.key}>{capitalize(level.key)}
+            </Radio.Button>
+        )
+        children.push(child);
     }
 
-    handleTooltipClick = () => {
-        this.setState({showTooltip: !this.state.showTooltip})
-    }
+    const title = sevCount === 1 ? 'SEVERITY' : ('Severity for ' + formatTitle(scenario.name));
 
-    handleMouseEnter = (e) => {this.props.onSeverityHover(e)}
-
-    handleMouseLeave= () => {this.props.onSeverityHoverLeave()}
-
-    render() {
-        const { severity, scenario, sevCount, isDisabled } = this.props; 
-        const title = sevCount === 1 ?
-            'SEVERITY' : ('Severity for ' + formatTitle(scenario.name));
-        return ( 
-            <div
-                onMouseEnter={() => this.handleMouseEnter(scenario.name)}
-                onMouseLeave={() => this.handleMouseLeave(scenario.name)}>
-                <div className="param-header">{title}
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                        >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                            <span className="tooltip-text">
+    return ( 
+        <div
+            onMouseEnter={() => handleMouseEnter(scenario.name)}
+            onMouseLeave={() => handleMouseLeave(scenario.name)}>
+            <div className="param-header">
+                {title}
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}>
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                        <span className="tooltip-text">
                             The high, medium, and low severity labels correspond 
                             to 1%, 0.5%, and 0.25% infection fatality ratios (IFR), 
                             and 10%, 5% and 2.5% hospitalization rates, respectively. 
                             Note that models are simulated independently for each severity level, 
                             even for the same model scenario.
-                            </span> }
-                        </div>
-                    </TooltipHandler>
-                </div>
-                <Radio.Group
-                    value={severity.key} 
-                    style={styles.Severity}
-                    disabled={isDisabled}
-                    onChange={this.handleChange}>
-                    {this.state.children.map(child => child.button)}
-                </Radio.Group>
+                        </span>}
+                    </div>
+                </TooltipHandler>
             </div>
-        )
-    }
-}
+            <Radio.Group
+                value={severity.key} 
+                style={styles.Severity}
+                disabled={isDisabled}
+                onChange={handleChange}>
+                {children.map(child => child.button)}
+            </Radio.Group>
+        </div>
+    );
+};
 
-export default Severity
+Severity.propTypes = { 
+    existingSevs: PropTypes.array.isRequired, 
+    scenario: PropTypes.object.isRequired, 
+    onSeverityClick: PropTypes.func.isRequired, 
+    onSeverityHover: PropTypes.func.isRequired, 
+    onSeverityHoverLeave: PropTypes.func.isRequired, 
+    isDisabled: PropTypes.bool.isRequired, 
+    sevCount: PropTypes.number.isRequired, 
+    severity: PropTypes.object.isRequired, 
+};
+
+export default Severity;
