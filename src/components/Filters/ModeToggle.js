003f5d50def53d1a3c298b1a188afa3ee047b702
Timothy Willard <timothywillard22@gmail.com>
2024-05-29 12:41:42 -0400

    Converted the ModeToggle from using React's class interface to the function interface

--- src/components/Filters/ModeToggle.js
+++ src/components/Filters/ModeToggle.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useCallback, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 import { Radio } from 'antd';
 import { InfoCircleTwoTone } from '@ant-design/icons';
@@ -6,65 +7,67 @@ import { InfoCircleTwoTone } from '@ant-design/icons';
 import TooltipHandler from './TooltipHandler';
 import { styles } from '../../utils/constants';
 
-class ModeToggle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTooltip: false
-        }
-    }
+const ModeToggle = ({
+    showConfBounds,
+    onConfClick
+}) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
 
-    handleTooltipClick = () => {
-        this.setState({ showTooltip: !this.state.showTooltip })
-    }
+    const handleTooltipClick = useCallback(() => {
+        setShowTooltip(!showTooltip);
+    }, [ showTooltip ]);
 
-    render() {
-        const value = this.props.showConfBounds ? "confidence" : "exceedance";
-        return (
-            <Fragment>
-                <div className="param-header">MODE
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                        >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                            <span className="tooltip-text">
-                                In “Confidence Bounds” mode, you can see a time-averaged 
-                                median line and 10-90% prediction interval ribbon for all 
-                                model simulations overlaid in green on top of the individual simulations. 
-                                <br /><br />
-                                In “Threshold Exceedance” mode, you can use the Threshold and Date 
-                                Threshold sliders to change values and dates to determine how likely 
-                                a given indicator will exceed a certain threshold number by 
-                                a given threshold date. Simulation curves that exceed the 
-                                designated threshold will appear red, while the rest 
-                                of the curves will be green.
-                            </span> }
-                        </div>
-                    </TooltipHandler>
-                </div>
-                <Radio.Group
-                    value={value}
-                    style={{ width: '70%', display: 'flex' }}
-                    onChange={this.props.onConfClick}>
-                    <Radio.Button
-                        key="confidence"
-                        style={styles.Radio}
-                        value="confidence">
-                        Confidence Bounds
-                    </Radio.Button>
-                    <Radio.Button
-                        key="exceedance"
-                        style={styles.Radio}
-                        value="exceedance">
-                        Threshold Exceedance
-                    </Radio.Button>
-                </Radio.Group>
-            </Fragment>
-        )
-    }
-}
+    const value = showConfBounds ? "confidence" : "exceedance";
+
+    return (
+        <Fragment>
+            <div className="param-header">
+                MODE
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}>
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                        <span className="tooltip-text">
+                            In “Confidence Bounds” mode, you can see a time-averaged 
+                            median line and 10-90% prediction interval ribbon for all 
+                            model simulations overlaid in green on top of the individual simulations. 
+                            <br /><br />
+                            In “Threshold Exceedance” mode, you can use the Threshold and Date 
+                            Threshold sliders to change values and dates to determine how likely 
+                            a given indicator will exceed a certain threshold number by 
+                            a given threshold date. Simulation curves that exceed the 
+                            designated threshold will appear red, while the rest 
+                            of the curves will be green.
+                        </span>}
+                    </div>
+                </TooltipHandler>
+            </div>
+            <Radio.Group
+                value={value}
+                style={{ width: '70%', display: 'flex' }}
+                onChange={onConfClick}>
+                <Radio.Button
+                    key="confidence"
+                    style={styles.Radio}
+                    value="confidence">
+                    Confidence Bounds
+                </Radio.Button>
+                <Radio.Button
+                    key="exceedance"
+                    style={styles.Radio}
+                    value="exceedance">
+                    Threshold Exceedance
+                </Radio.Button>
+            </Radio.Group>
+        </Fragment>
+    );
+};
+
+ModeToggle.propTypes = {
+    showConfBounds: PropTypes.bool.isRequired,
+    onConfClick: PropTypes.func.isRequired,
+};
 
 export default ModeToggle;
