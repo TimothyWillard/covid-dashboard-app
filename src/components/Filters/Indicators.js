4501df3b44e55c55d75910b3921dab07ab762430
Timothy Willard <timothywillard22@gmail.com>
2024-05-29 10:38:10 -0400

    Converted the Indicators component to use React's function interface instead of the class interface

--- src/components/Filters/Indicators.js
+++ src/components/Filters/Indicators.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+
 import { Select } from 'antd';
+const { Option } = Select; 
 import { InfoCircleTwoTone } from '@ant-design/icons';
+
 import { styles } from '../../utils/constants';
 import TooltipHandler from './TooltipHandler';
 
-class Indicators extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            children: [],
-            showTooltip: false
-        }
-    }
+const Indicators = ({
+    indicators,
+    indicator,
+    onIndicatorClick
+}) => {
+    const [ showTooltip, setShowTooltip ] = useState(false);
 
-    componentDidMount() {
-        const children = [];
-        const { Option } = Select;
+    const handleTooltipClick = useCallback(() => {
+        setShowTooltip(!showTooltip);
+    }, [ showTooltip ]);
 
-        for (let indicator of this.props.indicators) {
+    const handleChange = useCallback((e) => {
+        const item = indicators.filter(i => i.key === e)[0];
+        onIndicatorClick(item);
+    }, [ indicators, onIndicatorClick ]);
+
+    let children = [];
+    if (indicators) {
+        for (let indicator of indicators) {
             const child = {
                 key: `${indicator.key}-indicator`,
                 button: []
@@ -25,56 +34,49 @@ class Indicators extends Component {
             child.button.push(
                 <Option
                     key={`${indicator.key}-indicator`}
-                    value={indicator.key}>{indicator.name}
+                    value={indicator.key}>
+                    {indicator.name}
                 </Option>
             );
             children.push(child);
         }
-
-        this.setState({ children })
     }
 
-    handleTooltipClick = () => {
-        this.setState({ showTooltip: !this.state.showTooltip })
-    }
-
-    handleChange = (e) => {
-        const item = this.props.indicators.filter(indicator => indicator.key === e)[0];
-        this.props.onIndicatorClick(item);
-    }
-
-    render() {
-        const { indicator } = this.props;
-        return (
-            <div>
-                <div className="param-header">INDICATOR    
-                    <TooltipHandler
-                        showTooltip={this.state.showTooltip}
-                        onClick={this.handleTooltipClick}
-                        >
-                        <div className="tooltip">
-                            &nbsp;<InfoCircleTwoTone />
-                            {this.state.showTooltip &&
-                            <span className="tooltip-text">
-                                Indicators are calculated after accounting for the 
-                                appropriate time delays and probabilities of 
-                                transitioning into a given state 
-                                (e.g., initial infection to hospitalization).
-                            </span> }
-                        </div>
-                    </TooltipHandler>
-                </div>
-                <Select
-                    // re-render every time indicator key changes
-                    key={indicator.key}
-                    defaultValue={indicator.key}
-                    style={styles.Selector}
-                    onChange={this.handleChange}>
-                    {this.state.children.map(child => child.button)}
-                </Select>
+    return (
+        <div>
+            <div className="param-header">
+                INDICATOR    
+                <TooltipHandler
+                    showTooltip={showTooltip}
+                    onClick={handleTooltipClick}>
+                    <div className="tooltip">
+                        &nbsp;<InfoCircleTwoTone />
+                        {showTooltip &&
+                        <span className="tooltip-text">
+                            Indicators are calculated after accounting for the 
+                            appropriate time delays and probabilities of 
+                            transitioning into a given state 
+                            (e.g., initial infection to hospitalization).
+                        </span>}
+                    </div>
+                </TooltipHandler>
             </div>
-        )
-    }
-}
+            <Select
+                // re-render every time indicator key changes
+                key={indicator.key}
+                defaultValue={indicator.key}
+                style={styles.Selector}
+                onChange={handleChange}>
+                {children.map(child => child.button)}
+            </Select>
+        </div>
+    );
+};
+
+Indicators.propTypes = {
+    indicators: PropTypes.array.isRequired,
+    indicator: PropTypes.object.isRequired,
+    onIndicatorClick: PropTypes.func.isRequired,
+};
 
 export default Indicators;
