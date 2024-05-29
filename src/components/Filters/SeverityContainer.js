9fc5d91af289581a32cf7ec6820afbf1744e979f
Timothy Willard <timothywillard22@gmail.com>
2024-05-29 10:24:21 -0400

    Modernize the SeverityContainer component to use React's function interface

--- src/components/Filters/SeverityContainer.js
+++ src/components/Filters/SeverityContainer.js
@@ -1,84 +1,76 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+
 import Severity from '../Filters/Severity';
 
-class SeverityContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            children: []
+const SeverityContainer = ({ 
+    scenarioList, 
+    scenarioMap, 
+    severityList, 
+    indicator, 
+    onSeveritiesClick, 
+    onSeveritiesHover, 
+    onSeveritiesHoverLeave 
+}) => {
+    const handleSeverityClick = useCallback((i) => {
+        onSeveritiesClick(i);
+    }, [ onSeveritiesClick ]);
+
+    const handleSeverityHover = useCallback((i) => {
+        onSeveritiesHover(i);
+    }, [ onSeveritiesHover ]);
+
+    const handleSeverityHoverLeave = useCallback(() => {
+        onSeveritiesHoverLeave();
+    }, [ onSeveritiesHoverLeave ]);
+
+    const isDisabled = indicator.name === 'Infections' ? true : false;
+    let children = [];
+    if (scenarioList && scenarioList.length > 0) {
+        for (let i = 0; i < scenarioList.length; i++) {
+            const keyVal = `${severityList[i].key}_${scenarioList[i].key}`;
+            const child = {
+                'key': keyVal,
+                'scenario': scenarioList[i],
+                'severity': []
+            };
+            child.severity.push(
+                <Severity
+                    key={keyVal}
+                    severity={severityList[i]}
+                    scenario={scenarioList[i]}
+                    existingSevs={scenarioMap[scenarioList[i].key]}  // array of sev levels
+                    isDisabled={isDisabled}
+                    sevCount={severityList.length}
+                    onSeverityClick={handleSeverityClick}
+                    onSeverityHover={handleSeverityHover}
+                    onSeverityHoverLeave={handleSeverityHoverLeave}/>
+            );
+            children.push(child);
         }
     }
 
-    componentDidMount() {
-        const child = this.buildSeverity(0);
-        this.setState({ children: [child] })
-    }
+    return (
+        <div>
+            {children.map(child => {
+                return (
+                    <div key={child.key}>
+                        {child.severity}
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
 
-    componentDidUpdate(prevProps) {
-        const { scenarioList, scenarioMap, severityList, indicator } = this.props;
-        const newChildren = [];
-
-        if (prevProps.scenarioList !== scenarioList ||
-            prevProps.scenarioMap !== scenarioMap ||
-            prevProps.severityList !== severityList ||
-            prevProps.indicator !== indicator ) {
-
-            for (let i = 0; i < scenarioList.length; i++) {
-                const child = this.buildSeverity(i);
-                newChildren.push(child);
-            }
-            this.setState({ children: newChildren })
-        }
-    }
-
-    buildSeverity(i) {
-        const { scenarioList, scenarioMap, severityList, indicator } = this.props;
-        const keyVal = `${severityList[i].key}_${scenarioList[i].key}`;
-
-        // Infection values are the same across all severity
-        const isDisabled = indicator.name === 'Infections';
-
-        const child = {
-            'key': keyVal,
-            'scenario': scenarioList[i],
-            'severity': []
-        }
-        child.severity.push(
-            <Severity
-                key={keyVal}
-                severity={severityList[i]}
-                scenario={scenarioList[i]}
-                existingSevs={scenarioMap[scenarioList[i].key]}  // array of sev levels
-                isDisabled={isDisabled}
-                sevCount={severityList.length}
-                onSeverityClick={this.handleSeverityClick}
-                onSeverityHover={this.handleSeverityHover}
-                onSeverityHoverLeave={this.handleSeverityHoverLeave}
-            />
-        )
-        return child;
-    }
-
-    handleSeverityClick = (i) => { this.props.onSeveritiesClick(i) }
-
-    handleSeverityHover = (i) => { this.props.onSeveritiesHover(i) }
-
-    handleSeverityHoverLeave = () => { this.props.onSeveritiesHoverLeave() }
-
-    render() {
-        const { children } = this.state;
-        return (
-            <div>
-                {children.map(child => {
-                    return (
-                        <div key={child.key}>
-                            {child.severity}
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
-}
+SeverityContainer.propTypes = {
+    scenarioList: PropTypes.array.isRequired, 
+    scenarioMap: PropTypes.object.isRequired, 
+    severityList: PropTypes.array.isRequired, 
+    indicator: PropTypes.object.isRequired, 
+    onSeveritiesClick: PropTypes.func.isRequired, 
+    onSeveritiesHover: PropTypes.func.isRequired, 
+    onSeveritiesHoverLeave: PropTypes.func.isRequired, 
+};
 
 export default SeverityContainer;
