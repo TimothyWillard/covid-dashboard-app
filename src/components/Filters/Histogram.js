e5c426b9e0815ac5543bc9c493eea61321784f82
Timothy Willard <timothywillard22@gmail.com>
2024-05-29 13:27:34 -0400

    Added proptypes to the Histogram component and changed the tabs to 4 spaces

--- src/components/Filters/Histogram.js
+++ src/components/Filters/Histogram.js
@@ -1,87 +1,106 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
 import { bin, max, range } from 'd3-array';
 import { scaleLinear } from 'd3-scale';
-import { colors } from '../../utils/colors.js';
 
-export default function Histogram({ allSims, selectedSims, sortedSims, selected, r0min, r0max, height, step }) {
-  const [width, setWidth] = useState(0);
+import { colors } from '../../utils/colors';
 
-  function resizeHandler(e) {
-    const r0Slider = document.querySelector('.r0-slider');
-    if (r0Slider === null) {
-      setWidth(0);
+const Histogram = ({
+    selectedSims,
+    sortedSims,
+    r0min,
+    r0max,
+    height,
+    step
+}) => {
+    const [width, setWidth] = useState(0);
+
+    function resizeHandler() {
+        const r0Slider = document.querySelector('.r0-slider');
+        if (r0Slider === null) {
+            setWidth(0);
+        }
+        const width = r0Slider.clientWidth;
+        if (width === null) {
+            setWidth(0);
+        }
+        setWidth(width);
     }
-    const width = r0Slider.clientWidth;
-    if (width === null) {
-      setWidth(0);
+
+    useEffect(() => {
+        setWidth(document.querySelector('.r0-slider').clientWidth);
+        window.addEventListener('resize', resizeHandler);
+        return () => {
+            window.removeEventListener('resize', resizeHandler);
+        };
+    }, []);
+
+    const sorted_selected_sims = selectedSims.slice().sort((a, b) => a.r0 - b.r0);
+    const xScale = scaleLinear().domain([r0min, r0max]).range([0, width]).nice();
+    const yScale = scaleLinear().range([height, 1]);
+    const thresholds = range(r0min, r0max, step);
+
+    const binGenerator = bin()
+        .value(d => d.r0)
+        .domain(xScale.domain())
+        .thresholds(thresholds);
+
+    let bins = [];
+    let selectedBins = [];
+    if (sortedSims !== undefined) {
+        bins = binGenerator(sortedSims);
+        selectedBins = binGenerator(sorted_selected_sims);
+        yScale.domain([0, max(bins, d => d.length)]);
     }
-    setWidth(width);
-  }
 
-  useEffect(() => {
-    setWidth(document.querySelector('.r0-slider').clientWidth);
-    window.addEventListener('resize', resizeHandler);
-    return () => {
-      window.removeEventListener('resize', resizeHandler);
-    };
-  }, []);
-
-  const sorted_selected_sims = selectedSims.slice().sort((a, b) => a.r0 - b.r0);
-  const xScale = scaleLinear().domain([r0min, r0max]).range([0, width]).nice();
-  const yScale = scaleLinear().range([height, 1]);
-  const thresholds = range(r0min, r0max, step);
-
-  const binGenerator = bin()
-    .value(d => d.r0)
-    .domain(xScale.domain())
-    .thresholds(thresholds);
-
-  let bins = [];
-  let selectedBins = [];
-  if (sortedSims !== undefined) {
-    bins = binGenerator(sortedSims);
-    selectedBins = binGenerator(sorted_selected_sims);
-    yScale.domain([0, max(bins, d => d.length)]);
-  }
-
-  return (
-    <div>
-      {width && xScale &&
-        <svg width={width} height={height}>
-          <g>
-            {bins.map((b, i) => {
-              return (
-                <rect
-                  key={`hist-${i}`}
-                  x={xScale(b.x0)}
-                  y={yScale(b.length)}
-                  width={xScale(b.x1) - xScale(b.x0)}
-                  height={height - yScale(b.length)}
-                  fill="#ffffff"
-                  stroke={colors.chartBkgd}
-                >
-                </rect>
-              )
-            })}
-          </g>
-          <g>
-            {selectedBins.map((b, i) => {
-              return (
-                <rect
-                  key={`hist-${i}`}
-                  x={xScale(b.x0)}
-                  y={yScale(b.length)}
-                  width={xScale(b.x1) - xScale(b.x0)}
-                  height={height - yScale(b.length)}
-                  fill={colors.sliderBlue}
-                  stroke={colors.chartBkgd}
-                >
-                </rect>
-              )
-            })}
-          </g>
-        </svg>
-      }
-    </div>
-  );
+    return (
+        <div>
+            {width && xScale &&
+                <svg width={width} height={height}>
+                    <g>
+                        {bins.map((b, i) => {
+                            return (
+                                <rect
+                                    key={`hist-${i}`}
+                                    x={xScale(b.x0)}
+                                    y={yScale(b.length)}
+                                    width={xScale(b.x1) - xScale(b.x0)}
+                                    height={height - yScale(b.length)}
+                                    fill="#ffffff"
+                                    stroke={colors.chartBkgd}>
+                                </rect>
+                            )
+                        })}
+                    </g>
+                    <g>
+                        {selectedBins.map((b, i) => {
+                            return (
+                                <rect
+                                    key={`hist-${i}`}
+                                    x={xScale(b.x0)}
+                                    y={yScale(b.length)}
+                                    width={xScale(b.x1) - xScale(b.x0)}
+                                    height={height - yScale(b.length)}
+                                    fill={colors.sliderBlue}
+                                    stroke={colors.chartBkgd}>
+                                </rect>
+                            )
+                        })}
+                    </g>
+                </svg>
+            }
+        </div>
+    );
 };
+
+Histogram.propTypes = {
+    selectedSims: PropTypes.array,
+    sortedSims: PropTypes.array,
+    r0min: PropTypes.number,
+    r0max: PropTypes.number,
+    height: PropTypes.number.isRequired,
+    step: PropTypes.number.isRequired,
+};
+
+export default Histogram;
