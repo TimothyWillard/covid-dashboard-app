338073b3e7483c27469a06508beb099d9f39889d
Timothy Willard <timothywillard22@gmail.com>
2024-06-19 16:46:23 -0400

    Added prop types validation for the chart CalloutLabel component

--- src/components/Chart/CalloutLabel.js
+++ src/components/Chart/CalloutLabel.js
@@ -1,27 +1,38 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
 import { addCommas } from '../../utils/utils.js';
 
-function CalloutLabel(props) {
+const CalloutLabel = ({ classProps, scenario, median, label, tenth, ninetyith }) => {
     return (
         <Fragment>
-          <div className={props.classProps}>
-              <span className="bold">{props.scenario}</span>&nbsp;scenario:&nbsp;
+          <div className={classProps}>
+              <span className="bold">{scenario}</span>&nbsp;scenario:&nbsp;
               <span className="bold">50%</span>
-              &nbsp;{`chance of `}<span className="bold">{addCommas(Math.ceil(props.median))}</span>
-              &nbsp;{`${props.label}`}
+              &nbsp;{`chance of `}<span className="bold">{addCommas(Math.ceil(median))}</span>
+              &nbsp;{`${label}`}
               {/* from `}<span className="bold">{startDate}</span>
               &nbsp;{`to `}<span className="bold">{endDate}</span> */}
           </div>
-          <div className={props.classProps}>
+          <div className={classProps}>
             <span className="bold">90%</span>
-              &nbsp;{`chance of `}<span className="bold">{addCommas(Math.ceil(props.tenth))}</span>
-              &nbsp;{`to `}<span className="bold">{addCommas(Math.ceil(props.ninetyith))}</span>
-              &nbsp;{`${props.label}`} 
+              &nbsp;{`chance of `}<span className="bold">{addCommas(Math.ceil(tenth))}</span>
+              &nbsp;{`to `}<span className="bold">{addCommas(Math.ceil(ninetyith))}</span>
+              &nbsp;{`${label}`} 
               {/* from `}<span className="bold">{startDate}</span>
               &nbsp;{`to `}<span className="bold">{endDate}</span> */}
           </div>
         </Fragment>
         )
+};
+
+CalloutLabel.propTypes = {
+  classProps: PropTypes.string.isRequired,
+  scenario: PropTypes.string.isRequired,
+  median: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
+  tenth: PropTypes.number.isRequired,
+  ninetyith: PropTypes.number.isRequired,
 }
 
-export default CalloutLabel
+export default CalloutLabel;
